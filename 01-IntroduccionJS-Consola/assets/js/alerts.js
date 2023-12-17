f1296f33f142c6954bae9d2cfbc963bf394ed095
1495michael <mike_g.hbl95@hotmail.com>
2023-12-17 00:31:10 -0600

    Introduccion a javascript

---
+++ 01-IntroduccionJS-Consola/assets/js/alerts.js
@@ -0,0 +1,5 @@
+alert('Hola mundo');
+let nombre = prompt('¿Cual es tu nombre?', 'Sin nombre');
+console.log('Hola '+nombre);
+const seleccion = confirm('estas seguro de borrar esto?');
+console.log(seleccion)
